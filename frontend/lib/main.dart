53892b198441a64cccf03c275116a2049c1c7122
Ugo <ugo.prenat@gmail.com>
2023-01-16 10:22:29 +0100

    routes init

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:maating/pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:maating/pages/login_page.dart';
+import 'package:maating/pages/map_page.dart';
+import 'package:maating/pages/register_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,16 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final textTheme = Theme.of(context).textTheme;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {},
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/map': (context) => const MapPage(),
+      },
       theme: ThemeData(
           textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-            bodyText1: GoogleFonts.poppins(textStyle: textTheme.bodyText1))
-      ),
+              bodyText1: GoogleFonts.poppins(textStyle: textTheme.bodyText1))),
       home: const HomePage(),
     );
   }
