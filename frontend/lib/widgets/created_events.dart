5c4b0db09aaaba55b03c2906e92e4ad0e5db213b
Yanis JEDRZEJCZAK <yjedrzejczak@unblocked-group.com>
2023-03-20 15:41:11 +0100

    Add: Pages and Widget for Events created/joined by User

---
+++ frontend/lib/widgets/created_events.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:maating/services/requestManager.dart';
+import 'package:maating/widgets/eventCard.dart';
+
+import '../models/event.dart';
+
+class CreatedEvents extends StatefulWidget {
+  const CreatedEvents({super.key});
+
+  @override
+  State<CreatedEvents> createState() => _CreatedEventsState();
+}
+
+class _CreatedEventsState extends State<CreatedEvents> {
+  late Future<List<dynamic>> eventsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    eventsFuture = getEventsByOrganizerId("641852e4f92f960c8b1217a8");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Text(
+                'Évènements crées',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: Center(
+                child: FutureBuilder<List<Event>>(
+                  future: getEventsByOrganizerId("641852e4f92f960c8b1217a8"),
+                  builder: (
+                      BuildContext context,
+                      AsyncSnapshot<List<Event>> snapshot,
+                      ) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 40),
+                          child: CircularProgressIndicator(
+                            valueColor:
+                            AlwaysStoppedAnimation<Color>(Colors.grey[500]!),
+                          ),
+                        ),
+                      );
+                    }
+                    if (snapshot.hasData) {
+                      return Center(
+                        child: snapshot.data!.isEmpty
+                            ? Padding(
+                          padding: const EdgeInsets.only(top: 40),
+                          child: Text(
+                            'Aucun évènement trouvé',
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                            ),
+                          ),
+                        )
+                            : ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) => EventCard(
+                            event: snapshot.data![index],
+                          ),
+                          itemCount: snapshot.data!.length,
+                        ),
+                      );
+                    } else {
+                      return const Text('Une erreur est survenue');
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget ListViewBuilder(List<Event> events) {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) => EventCard(
+          event: events[index],
+        ),
+        itemCount: events.length,
+      ),
+    );
+  }
+}
