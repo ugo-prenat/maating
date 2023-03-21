6ddc26d0a181446c66c01e9f6b37cfdb844b3cef
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2023-03-21 17:31:22 +0100

    Update register_page2.dart

--- frontend/lib/pages/register_page2.dart
+++ frontend/lib/pages/register_page2.dart
@@ -205,13 +205,15 @@ class _RegisterPage2State extends State<RegisterPage2> {
                             }
                           },
                           style: ElevatedButton.styleFrom(
-                              padding: const EdgeInsets.only(left: 25),
-                              fixedSize: const Size(150, 50),
-                              backgroundColor: Colors.white,
-                              side: const BorderSide(
-                                  width: 2,
-                                  color: Colors.white,
-                                  strokeAlign: StrokeAlign.center)),
+                            padding: const EdgeInsets.only(left: 25),
+                            fixedSize: const Size(150, 50),
+                            backgroundColor: Colors.white,
+                            side: const BorderSide(
+                              width: 2,
+                              color: Colors.white,
+                              strokeAlign: BorderSide.strokeAlignCenter,
+                            ),
+                          ),
                           child: Row(
                             children: [
                               const Text(
