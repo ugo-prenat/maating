1ab2a22b7f866fc728338e772302d9428bb0d840
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2023-05-11 15:11:16 +0200

    Update users.controllers.js

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -66,11 +66,7 @@ const createUser = async (req, res) => {
     .catch((error) => res.status(500).json(error));
 };
 const updateUser = (req, res) => {
-  const data = req.body;
-
-  if (data.password) data.password = hashPassword(data.password);
-
-  return Users.findByIdAndUpdate(req.params.id, data, { new: true })
+  return Users.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then((user) => res.status(200).json(user))
     .catch((error) => res.status(500).json({ error }));
 };
